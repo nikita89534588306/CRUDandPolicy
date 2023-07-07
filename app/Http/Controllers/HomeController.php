f696e0bf76e4294771077b1d33f1e5b7a5c95040
Nikita Balashov <noWorkingEmail@mail.ru>
2023-07-07 16:46:33 +0500

    фейковые статьи выведены на главную страницу

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+
+        $posts = Post::query()
+            ->select('title', 'content')
+            ->limit(10)->get();
+            
+        return view('home')->with('posts', $posts);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,21 @@
         @include('templates.header')
     </header>
     <main>
-        
+        <div class='container'>
+            <div class='news-list row flex-column align-items-center'>
+                <h1 class='news-list-header m-3 text-center'>Последние новости</h1>
+                @foreach($posts as $singlePost)
+                    <div class="card col-6 mb-4">
+                        <div class="card-body ">
+                            <h5 class="card-title">{{ $singlePost->title}}</h5>
+                            <p class="card-text">
+                                {{  (strlen($singlePost->content) > 150) ? mb_strimwidth($singlePost->content, 0, 150, "...") : $singlePost->content ;  }}
+                            </p>
+                            <a href="#" class="btn btn-primary">Читать статью</a> 
+                        </div>
+                    </div>       
+                @endforeach
+            </div>       
+        </div>  
     <main>
 </x-main-layout>
