bc4e939ec4c3f95db6926eb3e09f898d03596ade
Nikita Balashov <noWorkingEmail@mail.ru>
2023-07-07 16:08:01 +0500

    создана модель постов и добавлены фейковые данные

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Post::class;
+    
+    public function definition(): array
+    {
+        
+
+        $valueWordTitile = fake()->numberBetween(5, 10);
+        $valueParagraphsInText = fake()->numberBetween(1, 5);
+        return [
+            'title' => fake()->sentence($valueWordTitile),
+            'content'=> fake()->paragraphs($valueParagraphsInText, true)
+        ];
+    }
+}
